add23a82edc6d723705d87611a8ebec39fe0968d
Trevor <halvas_trevor@halvas.ca>
2020-02-20 14:03:02 -0500

    adding mailer functionality - needs to be fixed

---
+++ config/mailcreds.js
@@ -0,0 +1,7 @@
+const mailcreds = {
+    service: 'gmail',
+    user: 'tvrfanshawe@gmail.com', // your email (gmail) goes here, don't use mine
+    pass: 'Fanshawe1$' // see above
+}
+
+module.exports = mailcreds;
--- public/js/components/DownloadComponent.js
+++ public/js/components/DownloadComponent.js
@@ -1,6 +1,21 @@
 export default {
     template: `
-        <h1>This is the download component</h1>
+        <section>
+            <h1 class="hidden">This is the download component</h1>
+
+            <form v-on:submit.prevent="handleMail" action="">
+                <label for="usermail">Please enter your email address:</label>
+                <input type="email" name="usermail" required>
+
+                <label for="name">Please enter your company name (optional)</label>
+                <input type="text" name="company" required>
+
+                <label for="message">Please enter your message:</label>
+                <textarea name="message" id="user-message" cols="30" rows="10"></textarea>
+
+                <input type="submit" value="Send!" class="submit-button">
+            </form>           
+        </section>
     `,
 
     data: function() {
@@ -12,5 +27,46 @@ export default {
 
     created: function() {
         console.log('form ready');
+    },
+
+    methods: {
+        handleMail() {
+            debugger;
+
+            let formdata = new FormData(document.querySelector('form')),
+            maildata = {};
+
+            // parse the form data
+            // and populate the maildata object with the input values (the formdata entries)
+            for (let [key, value] of formdata.entries()) {
+                maildata[key] = value;
+            }
+
+            let url = `/mail`;
+
+            fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json, text/plain, */*',
+                    'Content-type': 'application/json'
+                },
+    
+                body: JSON.stringify(maildata)
+            })
+                .then(res => res.json())
+                .then(data => {
+                    // remove this when testing is done and everything is working
+                    console.log(data);
+    
+                    if (data.response.includes("OK")) {
+                        // we successfully sent an email via gmail and nodemailer!
+                        // flash success here, reset the form
+                        form.reset();
+                        alert("email was sent!"); // DO NOT use alerts. they are so hacky and gross.
+                    }
+                }) // this will be a success or fail message from the server
+            .catch((err) => console.log(err));
+            // end of our fetch call
+        } // end of handle mail function
     }
 }
