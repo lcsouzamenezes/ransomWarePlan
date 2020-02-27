a873a09cb4ed5ae51e3f5f736670e55ac4f45e61
Trevor <halvas_trevor@halvas.ca>
2020-02-26 19:05:29 -0500

    adding new styles and images

--- public/js/components/HomeComponent.js
+++ public/js/components/HomeComponent.js
@@ -4,9 +4,21 @@ import DownloadComponent from "./DownloadComponent.js";
 export default {
     template: `
     <div>
-        <section>
+        <section class="home-wrapper">
             <h1 class="hidden">Home page</h1>
-            <h2>How to build a Backup and Disaster Recovery plan</h2>
+            <h2 id="main-header">How to build a Backup and Disaster Recovery Plan</h2>
+
+            <div class="image-bar">
+                <div>
+                <img src="images/disaster.jpg" alt="disaster image">
+                </div>
+                <div>
+                <img src="images/cyberattack.jpg" alt="disaster image">
+                </div>
+                <div>
+                <img src="images/coffee_spill.jpg" alt="disaster image">
+                </div>
+            </div>
 
             <p class="intro">
                 Unforeseen natural disasters, human errors or cyberattacks can cause you to lose access to critical information you need to run your business. Preparing your business to protect itself against the increasing threat of Ransomware attacks just makes sense. 
@@ -16,7 +28,8 @@ export default {
                 Building an effective plan will help you ensure business continuity and avoid preventable damage to your business and your reputation. This knowledge guide explains the what, why and how to protect yourself and your business.
             </p>
 
-            <h3>This FREE guide includes:</h3>
+            <h3 class="subhead">This FREE guide includes:</h3>
+
             <ul class="benefits">
                 <li>
                     5 benefits of building a Ransomware plan
@@ -30,7 +43,7 @@ export default {
                     Strategies to maintain business continuity after a disaster strikes
                 </li>
             </ul>
-            <h2>Fill out the form below for access to our guide</h2>
+            <h3 class="subhead">Fill out the form below for access to our guide!</h3>
         </section>
 
         <rForm id="reg-form" class="form-section" @authsuccess="authsuccess"></rForm>
--- public/js/components/PdfComponent.js
+++ public/js/components/PdfComponent.js
@@ -1,6 +1,9 @@
 export default {
     template: `
+    <div>
         <h1>This is where the pdf would go</h1>
+        <embed src="pdf/ransomware.pdf" type="application/pdf" width="25%" height="25%"></embed>
+    </div>
     `,
 
     created: function() {
