debde6a6560bd1aa84dccf03afdc35621b5ab322
tvanrys <tvanrys@fanshaweonline.ca>
2020-03-03 10:06:24 -0500

    adding tweaks

--- public/js/components/ErrorComponent.js
+++ public/js/components/ErrorComponent.js
@@ -3,7 +3,13 @@ export default {
     <section class="error-page">
         <h1>404: Page Not Found</h1>
         <h4>This is a navigation disaster!</h4>
-        <a href="/">Click here to recover</a>
+        <a @click.prevent="redirectToHome" href="/">Click here to recover</a>
     </section>
-    `
+    `,
+
+    methods: {
+        redirectToHome() {
+            this.$router.push("/");
+        }
+    }
 }
